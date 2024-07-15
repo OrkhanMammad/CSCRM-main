42561eb34178ef14d786bbdd5e4f51bbc9e83b99
User <User@DESKTOP-HB55BHG>
2024-07-15 11:50:50 +0400

    11.50

---
+++ CSCRM/Controllers/ConfirmationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSCRM.Controllers
+{
+    public class ConfirmationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+//
