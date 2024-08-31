bdc31782af8a894029d65aced35a157d6c839353
ACER <ACER@DESKTOP-CJBAJUG>
2024-08-31 22:53:43 +0400

    update after deploy v2

--- CSCRM/Program.cs
+++ CSCRM/Program.cs
@@ -61,7 +61,14 @@ builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+if (true)
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
